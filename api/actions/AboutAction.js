42018d05375fbd395df7de885c8fd4308c87fd76
Mick <mickfoely@163.com>
2019-06-18 01:06:51 +0800

    init

--- api/actions/AboutAction.js
+++ api/actions/AboutAction.js
@@ -2,7 +2,8 @@ const articleId = 'A8199a500911d11e9b72b6f397f38446d'
 const { Articles } = require('../models')
 class AboutAction {
 	get() {
-		return new Articles().get(articleId)
+		let { title, content } = new Articles().get(articleId)
+		return { title, content }
 	}
 }
 module.exports = { AboutAction }
