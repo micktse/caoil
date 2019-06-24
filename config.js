665792e74e93ccd4b80683624765d231633037c1
Mick <mickfoely@163.com>
2019-06-24 23:47:14 +0800

    init

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ let development = /^supervisor/.test(process.env.npm_lifecycle_script)
 let debug = !process.env.npm_lifecycle_script || /--inspect-brk/.test(process.env.npm_lifecycle_script)
 console.info(development ? '开发模式' : debug ? '调试模式' : '生产模式')
 module.exports = {
-	http_port: development || debug ? 2019 : 2019,
+	http_port: development || debug ? 80 : 80,
 	https_port: 443,
 	development,
 	debug,
-	https_key: undefined,
-	https_cert: undefined,
-	host_name: 'www.sureking.cn',
+	https_key: '2404076_caoilusa.com.key',
+	https_cert: '2404076_caoilusa.com.pem',
+	host_name: 'www.caoilusa.com',
 	sign_in_url: 'login.html',
 	main_url: '',
 	home_url: 'home.html',
