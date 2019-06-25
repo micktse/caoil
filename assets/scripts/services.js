5e3c4a3233cc070e81affdfefa3a9ad1c566916a
Mick <mickfoely@163.com>
2019-06-25 11:47:26 +0800

    init

--- assets/scripts/services.js
+++ assets/scripts/services.js
@@ -1,3 +1,35 @@
+function load() {
+	$.post('/sites.api', { id: 'Cfedd7040915a11e992b55796fb705920', invoke: 'loadArticles' }, function(res) {
+		for (var i = 0; i < res.data.length; i++) {
+			$('.faq').append(
+				$('<div>')
+					.attr('class', 'item')
+					.append($('<div>').attr('class', 'logo'))
+					.append(
+						$('<div>')
+							.attr('class', 'faq')
+							.append(
+								$('<div>')
+									.attr('class', 'question')
+									.html(res.data[i].title)
+							)
+							.append(
+								$('<div>')
+									.attr('class', 'answer')
+									.html(res.data[i].content)
+							)
+					)
+			)
+		}
+	})
+	// <div class="item">
+	// 	<div class="logo" />
+	// 	<div class="faq">
+	// 		<div class="question">This's the question. What's caoil?</div>
+	// 		<div class="answer">This is the answer! a oil brand which is best oil.</div>
+	// 	</div>
+	// </div>
+}
 var tabs = {
 	faq: '',
 	points: '',
@@ -21,4 +53,5 @@ $(function() {
 	} else {
 		changeTab($('#faq'))
 	}
+	load()
 })
